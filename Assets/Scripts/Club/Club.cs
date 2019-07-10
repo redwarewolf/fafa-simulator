f9a5211dd8201b403405b861ef94b97241d8e4a3
Pedro Jara <pedro.jara@wolox.com.ar>
2019-07-10 20:30:02 -0300

    More

--- Assets/Scripts/Club/Club.cs
+++ Assets/Scripts/Club/Club.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Club : MonoBehaviour
 {
   private List<FootballPlayer> footballPlayers;
+
+
+  public int power(){
+    return footballPlayers.Select( x => x.power()).Sum();
+  }
 }
